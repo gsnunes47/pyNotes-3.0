6889ad071845a741b53395dc57b3baef9a31e8c7
Gustavo Nunes <gustavompn06@gmail.com>
2023-09-04 19:16:24 -0300

    Criação da pagina de cadastro e implementação do formulário na mesma.

--- pynotes/routes.py
+++ pynotes/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from pynotes import app
-from pynotes.forms import SignIn
+from pynotes.forms import SignIn, SignUp
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = SignIn()
     return render_template('home.html', form=form)
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    form = SignUp()
+    return render_template('cadastro.html', form=form)
