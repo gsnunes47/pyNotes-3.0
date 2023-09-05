5ec48a80bc1f3b89fb84c513ca150b1d0b059ffe
Gustavo Nunes <gustavompn06@gmail.com>
2023-09-05 11:20:04 -0300

    Implementação da função de login e criação da pagina de perfil.

--- pynotes/routes.py
+++ pynotes/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from pynotes import app, bcrypt, database
 from pynotes.forms import SignIn, SignUp
 from pynotes.models import Usuario
@@ -6,6 +6,11 @@ from pynotes.models import Usuario
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = SignIn()
+    if form.is_submitted():
+        user = Usuario.query.filter_by(email=form.login.data).first()
+        senha = bcrypt.check_password_hash(user.senha, form.senha.data)
+        if user and senha:
+            return redirect(url_for("perfil", id_usuario=user.id))
     return render_template('home.html', form=form)
 
 @app.route('/cadastro', methods=['GET', 'POST'])
@@ -17,3 +22,8 @@ def cadastro():
         database.session.add(user)
         database.session.commit()
     return render_template('cadastro.html', form=form)
+
+@app.route('/perfil/<id_usuario>')
+def perfil(id_usuario):
+    user = Usuario.query.get(int(id_usuario))
+    return render_template("perfil.html", user=user)
